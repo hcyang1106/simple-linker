df3bca0faf43c8c19e0110b2b9c0b2fe3e32755e
howard <howard.yang@kiwigame.com>
2024-11-24 21:50:32 -0600

    1. add mark alive obj files functions
    2. add clear unused symbols and unused files function

--- pkg/linker/passes.go
+++ pkg/linker/passes.go
@@ -1,3 +1,38 @@
-package pkg
+package linker
 
-func ResolveSymbols
+func MarkLiveObjects(ctx *Context) {
+	roots := make([]*ObjectFile, 0)
+	for _, file := range ctx.Args.ObjFiles {
+		if file.IsAlive {
+			roots = append(roots, file)
+		}
+	}
+	for len(roots) > 0 {
+		roots = roots[0].MarkLiveObjects(ctx, roots)
+		roots = roots[1:]
+	}
+}
+
+func ClearSymbolsAndFiles(ctx *Context) {
+	ClearUnusedGlobalSymbols(ctx)
+	ClearUnusedFiles(ctx)
+}
+
+func ClearUnusedGlobalSymbols(ctx *Context) {
+	for _, file := range ctx.Args.ObjFiles {
+		if !file.IsAlive {
+			file.ClearUnusedGlobalSymbols(ctx)
+		}
+	}
+}
+
+func ClearUnusedFiles(ctx *Context) {
+	var i int = 0
+	for _, file := range ctx.Args.ObjFiles {
+		if file.IsAlive {
+			ctx.Args.ObjFiles[i] = file
+			i += 1
+		}
+	}
+	ctx.Args.ObjFiles = ctx.Args.ObjFiles[:i]
+}
