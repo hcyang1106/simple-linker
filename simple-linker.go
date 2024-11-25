8273f286e3498ee13736e78901082f7fdf20e2b7
howard <howard.yang@kiwigame.com>
2024-11-24 21:50:55 -0600

    modify test code

--- simple-linker.go
+++ simple-linker.go
@@ -2,98 +2,79 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"github.com/hcyang1106/simple-linker/pkg/utils"
 	"github.com/hcyang1106/simple-linker/pkg/linker"
+	"github.com/hcyang1106/simple-linker/pkg/utils"
+	"os"
+	"strings"
+
+	//"strings"
 )
+
+var version string
 
 // functions handle errs themselves
 func main() {
 	ctx := linker.NewContext()
-	parseArgs(ctx)
+	remaining := ctx.ParseArgs(ctx, version)
 
-	fmt.Println(ctx.Args.Output)
+	// if machine type not specified, find it in obj file
+	if ctx.Args.Machine == linker.MachineTypeNone {
+		for _, filename := range remaining {
+			if strings.HasPrefix(filename, "-") {
+				continue
+			}
+			// obj file
+			file := linker.NewFile(filename)
+			mType := linker.GetMachineTypeFromContent(file.Content)
+			if mType != linker.MachineTypeNone {
+				ctx.Args.Machine = mType
+				break
+			}
+		}
+	}
+	if ctx.Args.Machine != linker.MachineTypeRISCV64 {
+		utils.Fatal("Unsupported machine type...")
+	}
+
+	ctx.FillInObjFiles(remaining) // remaining contains specific libraries or obj files
+
+	fmt.Println(len(ctx.Args.ObjFiles))
+
+	linker.MarkLiveObjects(ctx)
+	fmt.Println("symbol count", len(ctx.SymbolMap))
+	linker.ClearSymbolsAndFiles(ctx)
+	fmt.Println("symbol count", len(ctx.SymbolMap))
+	fmt.Println(len(ctx.Args.ObjFiles))
+
+	for key, value := range ctx.SymbolMap {
+		if key == "puts" {
+			fmt.Println(value.File.File.Name)
+		}
+	}
 	os.Exit(0)
 
-	if len(os.Args) < 2 {
-		utils.Fatal("Invalid input arguments")
-	}
-
-	file := linker.NewFile(os.Args[1])
-	linker.MustHaveMagic(file.Content)
-
-	inputFile := linker.NewInputFile(file)
-	utils.Assert(len(inputFile.ElfSecHdrs) == 11)
-
-	// first section of elf file is usually empty
-	for _, shdr := range inputFile.ElfSecHdrs {
-		fmt.Println(linker.ElfGetName(inputFile.ShStrTab, shdr.Name))
-	}
-
-	inputFile.ParseSymTab()
-	utils.Assert(inputFile.FirstGlobal == 10)
-	utils.Assert(len(inputFile.ElfSyms) == 12)
-
-	// SymStrTab stores symbol names (array), and the Name field
-	// of symbol element stores the offset
-	for _, sym := range inputFile.ElfSyms {
-		fmt.Println(linker.ElfGetName(inputFile.SymStrTab, sym.Name))
-	}
-}
-
-func addDashes(option string) []string {
-	res := []string{}
-
-	if (len(option) == 1) {
-		res = append(res, "-" + option)
-	} else {
-		res = append(res, "-" + option, "--" + option)
-	}
-
-	return res
-}
-
-// usage: readFlag("help")
-// or readFlag("o")
-func readFlag(option string, currStr string) bool {
-	for _, opt := range addDashes(option) {
-		if currStr == opt {
-			return true
-		}
-	}
-
-	return false
-}
-
-func readOpt(option string, currStr string) bool {
-	for _, opt := range addDashes(option) {
-		if currStr == opt {
-			return true
-		}
-	}
-
-	return false
-}
-
-func parseArgs(ctx *linker.Context) []string {
-	args := os.Args
-
-	remaining := make([]string, 0)
-	for len(args) > 0 {
-		if readFlag("help", args[0]) {
-			fmt.Printf("usage: %s [options] files...\n", os.Args[0])
-			os.Exit(0)
-		} else if readOpt("o", args[0]) || readOpt("output", args[0]) {
-			args = args[1:]
-			if len(args) == 0 {
-				utils.Fatal("No output filename specified")
-			}
-			ctx.Args.Output = args[0]
-		} else {
-			remaining = append(remaining, args[0])
-		}
-		args = args[1:]
-	}
-
-	return remaining
+	//if len(os.Args) < 2 {
+	//	utils.Fatal("Invalid input arguments")
+	//}
+	//
+	//file := linker.NewFile(os.Args[1])
+	//linker.MustHaveMagic(file.Content)
+	//
+	//inputFile := linker.NewObjectFile(file)
+	//utils.Assert(len(inputFile.ElfSecHdrs) == 11)
+	//
+	//// first section of elf file is usually empty
+	//for _, shdr := range inputFile.ElfSecHdrs {
+	//	fmt.Println(linker.ElfGetName(inputFile.ShStrTab, shdr.Name))
+	//}
+	//
+	//inputFile.ParseSymTab()
+	//utils.Assert(inputFile.FirstGlobal == 10)
+	//utils.Assert(len(inputFile.ElfSyms) == 12)
+	//
+	//// SymStrTab stores symbol names (array), and the Name field
+	//// of symbol element stores the offset
+	//for _, sym := range inputFile.ElfSyms {
+	//	fmt.Println(linker.ElfGetName(inputFile.SymStrTab, sym.Name))
+	//}
 }
