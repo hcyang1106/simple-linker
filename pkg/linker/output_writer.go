4bb0701ba046a29ba8a4d3305ed00b6cfbd5d3e3
howard <howard.yang@kiwigame.com>
2025-02-19 22:24:07 -0600

    add common methods, functions used for telling section attributes

--- pkg/linker/output_writer.go
+++ pkg/linker/output_writer.go
@@ -1,27 +1,78 @@
 package linker
 
+import "debug/elf"
+
 type iOutputWriter interface {
 	GetShdr() *Shdr
 	CopyBuf(ctx *Context)
+	GetName() string
+	UpdateSize(ctx *Context)
+	GetShndx() int64
 }
 
 type OutputWriter struct {
-	Name string
-	Shdr Shdr
+	Name  string
+	Shdr  Shdr
+	Shndx int64
 }
 
 func NewOutputWriter() *OutputWriter {
-	return &OutputWriter {
-		Shdr: Shdr {
+	return &OutputWriter{
+		Shdr: Shdr{
 			AddrAlign: 1,
 		},
 	}
+}
+
+func (o *OutputWriter) GetName() string {
+	return o.Name
 }
 
 func (o *OutputWriter) GetShdr() *Shdr {
 	return &o.Shdr
 }
 
-func (o *OutputWriter) CopyBuf(ctx *Context) {
+func (o *OutputWriter) GetShndx() int64 {
+	return o.Shndx
+}
 
+func (o *OutputWriter) UpdateSize(ctx *Context) {
+	// left empty for successor to implement
+}
+
+func (o *OutputWriter) CopyBuf(ctx *Context) {
+	// left empty for successor to implement
+}
+
+func isTLS(o iOutputWriter) bool {
+	return o.GetShdr().Flags&uint64(elf.SHF_TLS) != 0
+}
+
+func isBSS(o iOutputWriter) bool {
+	return o.GetShdr().Type == uint32(elf.SHT_NOBITS) && !isTLS(o)
+}
+
+func isNOTE(o iOutputWriter) bool {
+	return o.GetShdr().Type == uint32(elf.SHT_NOTE) &&
+		o.GetShdr().Flags&uint64(elf.SHF_ALLOC) != 0
+}
+
+func isNONALLOC(o iOutputWriter) bool {
+	if o.GetShdr().Flags&uint64(elf.SHF_ALLOC) == 0 {
+		return true
+	}
+	return false
+}
+
+func outputWriterAttrToPhdrFlags(o iOutputWriter) uint32 {
+	// must be readable
+	flags := uint32(elf.PF_R)
+	write := o.GetShdr().Flags&uint64(elf.SHF_WRITE) != 0
+	if write {
+		flags |= uint32(elf.PF_W)
+	}
+	if o.GetShdr().Flags&uint64(elf.SHF_EXECINSTR) != 0 {
+		flags |= uint32(elf.PF_X)
+	}
+	return flags
 }
