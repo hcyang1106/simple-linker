5014e81706ef9f7e11f3f017258ddb7e897c1d36
howard <howard.yang@kiwigame.com>
2025-02-19 22:08:56 -0600

    modify copybuf, add getFlags and getEntryAddress

--- pkg/linker/output_ehdr_writer.go
+++ pkg/linker/output_ehdr_writer.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hcyang1106/simple-linker/pkg/utils"
 )
 
+// should setup size, offset before using
 type OutputEhdrWriter struct {
 	OutputWriter
 }
@@ -14,6 +15,7 @@ type OutputEhdrWriter struct {
 func NewOutputEhdrWriter() *OutputEhdrWriter {
 	return &OutputEhdrWriter{
 		OutputWriter{
+			Name: "ehdr",
 			Shdr: Shdr{
 				Flags:     uint64(elf.SHF_ALLOC),
 				Size:      uint64(EhdrSize),
@@ -21,6 +23,29 @@ func NewOutputEhdrWriter() *OutputEhdrWriter {
 			},
 		},
 	}
+}
+
+func getEntryAddress(ctx *Context) uint64 {
+	for _, osec := range ctx.OutputSections {
+		if osec.Name == ".text" {
+			return osec.Shdr.Addr
+		}
+	}
+	return 0
+}
+
+// check if there are compressed instructions or not (32 -> 16)
+func getFlags(ctx *Context) uint32 {
+	utils.Assert(len(ctx.Args.ObjFiles) > 0)
+	flags := ctx.Args.ObjFiles[0].GetEhdr().Flags
+	for _, obj := range ctx.Args.ObjFiles[1:] {
+		if obj.GetEhdr().Flags&EF_RISCV_RVC != 0 {
+			flags |= EF_RISCV_RVC
+			break
+		}
+	}
+
+	return flags
 }
 
 func (o *OutputEhdrWriter) CopyBuf(ctx *Context) {
@@ -31,16 +56,18 @@ func (o *OutputEhdrWriter) CopyBuf(ctx *Context) {
 	ehdr.Ident[elf.EI_VERSION] = uint8(elf.EV_CURRENT) // fixed usage
 	ehdr.Ident[elf.EI_OSABI] = 0
 	ehdr.Ident[elf.EI_ABIVERSION] = 0
-
+	ehdr.Flags = getFlags(ctx)
 	ehdr.Type = uint16(elf.ET_EXEC)
 	ehdr.Machine = uint16(elf.EM_RISCV)
 	ehdr.Version = uint32(elf.EV_CURRENT)
-	// TODO
+	ehdr.Entry = getEntryAddress(ctx)
 	ehdr.EhSize = uint16(EhdrSize)
 	ehdr.PhEntSize = uint16(PhdrSize)
-	// TODO
+	ehdr.ShOff = ctx.OutputShdrsWriter.Shdr.Offset
 	ehdr.ShEntSize = uint16(ShdrSize)
-
+	ehdr.PhOff = ctx.OutputPhdrsWriter.Shdr.Offset
+	ehdr.PhNum = uint16(ctx.OutputPhdrsWriter.Shdr.Size / uint64(PhdrSize))
+	ehdr.ShNum = uint16(ctx.OutputShdrsWriter.Shdr.Size / uint64(ShdrSize))
 	buf := bytes.Buffer{}
 	err := binary.Write(&buf, binary.LittleEndian, ehdr)
 	utils.MustNo(err)
